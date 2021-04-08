060a49eb4f3bfc6263375984f94f5ae056a50bc2
Viola Bai <bai.tian@northeastern.edu>
2021-04-08 10:38:11 -0700

    added grid

--- src/HTMLText/HTMLText.jsx
+++ src/HTMLText/HTMLText.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
+import styles from "./HTMLText.module.css";
 
 const HTMLText = (props) => {
-  return <div dangerouslySetInnerHTML={{ __html: props.text }} />;
+  return (
+    <div className={styles.txt}  dangerouslySetInnerHTML={{ __html: props.text }} />
+  );
 };
 
 HTMLText.propTypes = {
